c3354c24fab2c7caa9f8ab95de67bc9bd8127bee
hoangphuhung1998@gmail.com <hoangphuhung1998@gmail.com>
2019-10-27 21:07:38 +0700

    update

---
+++ Assets/script/camera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    GameObject player;
+    private Vector3 offset;
+    private const float MINX= -5f;
+    private const float MAXX= 5.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("zero");
+        offset=new Vector3(0, 0.3f, transform.position.z - player.transform.position.z);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = player.transform.position;
+        if (pos.x > MINX && pos.x < MAXX)
+            transform.position = pos + offset;
+        else if (pos.x < MINX)
+            transform.position = new Vector3(MINX, pos.y, pos.z) + offset;
+        else if (pos.x >MAXX)
+            transform.position = new Vector3(MAXX, pos.y, pos.z) + offset;
+    }
+}
---
+++ Assets/script/zero.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class zero : MonoBehaviour
+{
+    private float speed;
+	Rigidbody2D rb;
+    Animator animator;
+    SpriteRenderer sr;
+    Transform tf;
+    bool onGround;
+    int maxJump ;
+    int curJump ;
+    Transform health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxJump = 2;
+        curJump = 0;
+        speed = Time.deltaTime;             
+	    rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        tf = GetComponent<Transform>();
+        onGround = false;
+        health = GameObject.Find("health").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            sr.flipX = true;
+            if(onGround)
+                animator.Play("run");
+            tf.Translate(new Vector2(-speed, 0));
+        }
+            
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            sr.flipX = false;
+            if (onGround)
+                animator.Play("run");
+            tf.Translate(new Vector2(speed, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.X) && curJump<maxJump)
+        {
+            animator.Play("jump");
+            rb.AddForce(new Vector2(0, 200)); 
+            curJump++;
+        }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            animator.Play("attack");
+        }
+        if(Input.GetKeyDown(KeyCode.V))
+        {
+            animator.Play("thunder");
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            animator.Play("dash");
+            if (sr.flipX==true)
+                rb.AddForce(new Vector2(-140, 0));
+            else rb.AddForce(new Vector2(140, 0));
+        }
+        if (Input.GetKey(KeyCode.Z) && Input.GetKey(KeyCode.V))
+        {
+            animator.Play("hurricane");
+        }
+        if (Input.GetKeyDown(KeyCode.V) && !onGround)
+        {
+            animator.Play("ice");
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && Input.GetKeyDown(KeyCode.V))
+        {
+            animator.Play("flame");
+            rb.AddForce(new Vector2(0, 200));
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+            SceneManager.LoadScene(1);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "ground")
+        {
+            onGround = true;
+            curJump = 0;
+        }
+        if (col.gameObject.name == "enemy" || col.gameObject.name == "slashbeast"){
+            float curHealth= health.localScale.y;
+            curHealth-=0.2f;
+            if (curHealth <0) curHealth = 0;
+            health.localScale = new Vector3(0.35f, curHealth);
+        }
+            
+    }
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if(col.gameObject.name=="ground")  
+            onGround = false;
+    }
+
+}
